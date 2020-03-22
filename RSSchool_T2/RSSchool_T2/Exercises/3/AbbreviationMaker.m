d240153963116b0fb9042bbb4a670df23d0b7ca1
Ivan Budovich <56168159+Nikolssen@users.noreply.github.com>
2020-03-22 14:45:13 +0300

    feat: solve Task3

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,14 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+
+    NSCharacterSet *bSet = [NSCharacterSet characterSetWithCharactersInString:b];
+    NSCharacterSet *notBSet = [bSet invertedSet];
+ 
+    NSArray <NSString *> *arrayOfCommonCharacters = [[a uppercaseString] componentsSeparatedByCharactersInSet: notBSet];
+        NSString *result = [arrayOfCommonCharacters componentsJoinedByString:@""];
+        if ([result isEqualToString:b]) return @"YES";
+        else return @"NO";
+    
 }
 @end
