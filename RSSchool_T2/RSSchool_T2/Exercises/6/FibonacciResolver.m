091c480afce4d9ecdff224077a6ac164038fd447
Ivan Budovich <56168159+Nikolssen@users.noreply.github.com>
2020-03-22 14:47:08 +0300

    feat: solve Task6

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,31 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
+    NSMutableArray <NSNumber*> *FibonacciArr = [NSMutableArray new];
+    [FibonacciArr addObject:@0];
+    [FibonacciArr addObject:@1];
+    int i = 1;
+    NSMutableArray *result = [NSMutableArray new];
+    while (true)
+    {
+        if ([FibonacciArr[i-1] intValue] * [FibonacciArr[i] intValue] == [number intValue]) {
+            [result addObject:FibonacciArr[i-1]];
+            [result addObject:FibonacciArr[i]];
+            [result addObject:@1];
+            return result;
+        }
+        else  if ([FibonacciArr[i-1] intValue] * [FibonacciArr[i] intValue] > [number intValue]) {
+            
+            [result addObject:FibonacciArr[i-1]];
+            [result addObject:FibonacciArr[i]];
+            [result addObject:@0];
+            return result;
+            }
+        else {
+            [FibonacciArr addObject:[NSNumber numberWithInt:([FibonacciArr[i] intValue]+[FibonacciArr[i-1] intValue] )]];
+            i++;
+        }
+    }
     return @[];
 }
 @end
